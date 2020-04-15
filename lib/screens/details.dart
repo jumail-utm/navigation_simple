df7c9914b527273037d0b6d13158efec6c0877ab
Jumail <jumail@utm.my>
2020-04-16 07:35:33 +0800

    Disable AppBar and phone back buttons in DetailsScreen

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -14,49 +14,54 @@ class DetailsScreen extends StatefulWidget {
 class _DetailsScreenState extends State<DetailsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget._data.member.shortName),
-      ),
-      body: ListView.separated(
-        itemCount: criteria.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(criteria[index].title),
-          subtitle: Text(criteria[index].description),
-          trailing: DropdownButton<int>(
-            value: widget._data.points[index],
-            items: scales
-                .map(
-                  (scale) => DropdownMenuItem(
-                    value: scale.value,
-                    child: Text(scale.title),
-                  ),
-                )
-                .toList(),
-            onChanged: (newValue) =>
-                setState(() => widget._data.points[index] = newValue),
+    return WillPopScope(
+      onWillPop: () => Future.value(false),
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Container(),
+          title: Text(widget._data.member.shortName),
+          centerTitle: true,
+        ),
+        body: ListView.separated(
+          itemCount: criteria.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(criteria[index].title),
+            subtitle: Text(criteria[index].description),
+            trailing: DropdownButton<int>(
+              value: widget._data.points[index],
+              items: scales
+                  .map(
+                    (scale) => DropdownMenuItem(
+                      value: scale.value,
+                      child: Text(scale.title),
+                    ),
+                  )
+                  .toList(),
+              onChanged: (newValue) =>
+                  setState(() => widget._data.points[index] = newValue),
+            ),
+          ),
+          separatorBuilder: (context, index) => Divider(
+            color: Colors.grey,
           ),
         ),
-        separatorBuilder: (context, index) => Divider(
-          color: Colors.grey,
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FloatingActionButton.extended(
+              label: const Text('Save'),
+              icon: const Icon(Icons.check_circle),
+              heroTag: null,
+              onPressed: () => Navigator.of(context).pop(widget._data),
+            ),
+            FloatingActionButton.extended(
+              label: const Text('Cancel'),
+              icon: const Icon(Icons.cancel),
+              heroTag: null,
+              onPressed: () => Navigator.of(context).pop(null),
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          FloatingActionButton.extended(
-            label: const Text('Save'),
-            icon: const Icon(Icons.check_circle),
-            heroTag: null,
-            onPressed: () => Navigator.of(context).pop(widget._data),
-          ),
-          FloatingActionButton.extended(
-            label: const Text('Cancel'),
-            icon: const Icon(Icons.cancel),
-            heroTag: null,
-            onPressed: () => Navigator.of(context).pop(null),
-          ),
-        ],
       ),
     );
   }
